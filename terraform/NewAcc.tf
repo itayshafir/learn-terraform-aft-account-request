556d534d2aa975fb22bbec4a0f7fcb05f2901571
Itay Shafir <itaysh@amazon.com>
2024-08-26 20:38:04 +0300

    NewACC

---
+++ terraform/NewAcc.tf
@@ -0,0 +1,27 @@
+module "Account" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "itaysh+aft-vended2@amazon.com"
+    AccountName               = "aft2"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "itaysh+aft-vended2@amazon.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT2"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AW3S Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = ""
+}
